bd43514548af24f4d475fda747aa85b5a1e2e435
phalves23 <rm93693@fiap.com.br>
2022-11-13 17:08:55 -0300

    :sparkles: develop app

--- App.js
+++ App.js
@@ -1,11 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import Tarefa from "./src/Tarefa";
 export default function App() {
+  const [tarefa, setTarefa] = useState("");
+  const [lista, setLista] = useState([]);
+
+  function handleAdd() {
+    if (tarefa === "") {
+      return;
+    }
+    const dados = {
+      key: Date.now(),
+      item: tarefa,
+    };
+
+    setLista((oldArray) => [dados, ...oldArray]);
+    setTarefa("");
+  }
+
+  function handleDelete(item) {
+    let filtroItem = lista.filter((tarefa) => {
+      return tarefa.item !== item;
+    });
+    setLista(filtroItem);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Tarefas</Text>
+
+      <View style={styles.containerInput}>
+        <TextInput
+          style={styles.input}
+          value={tarefa}
+          onChangeText={(text) => setTarefa(text)}
+          placeholder="Digite sua tarefa..."
+        />
+        <TouchableOpacity onPress={handleAdd} style={styles.buttonAdd}>
+          <FontAwesome name="plus" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={lista}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <Tarefa data={item} deleteItem={() => handleDelete(item.item)} />
+        )}
+        style={styles.list}
+      />
     </View>
   );
 }
@@ -13,8 +64,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#22272E",
+    paddingTop: 28,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    color: "#FFF",
+    marginTop: "5%",
+    paddingStart: "5%",
+    marginBottom: 12,
+  },
+  containerInput: {
+    flexDirection: "row",
+    width: "100%",
+    height: 44,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 22,
+  },
+  input: {
+    width: "75%",
+    backgroundColor: "#FBFBFB",
+    height: 44,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+  },
+  buttonAdd: {
+    width: "15%",
+    height: 44,
+    backgroundColor: "#73f7ff",
+    marginLeft: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 4,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    paddingStart: "4%",
+    paddingEnd: "4%",
   },
 });
